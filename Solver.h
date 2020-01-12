a950e129ad4c2f671c27b98ecd171b229308f6ce
hodyah <hodyahadler@gmail.com>
2020-01-12 19:08:40 +0200

    Merge branch 'master' of /home/hodyah/CLionProjects/SearchingAlgoServer with conflicts.

--- Solver.h
+++ Solver.h
@@ -5,12 +5,15 @@
 #ifndef SEARCHINGALGOSERVER_SOLVER_H
 #define SEARCHINGALGOSERVER_SOLVER_H
 
+namespace SolverProblem{
+    template<typename Problem, typename Solution> class Solver {
+        virtual Solution* solve(Problem*) = 0;
+    };
 
-template<typename Problem, typename Solution> class Solver {
-    
-public:
-    virtual Solution* solve(Problem*) = 0;
+
 };
+
+
 
 
 #endif //SEARCHINGALGOSERVER_SOLVER_H
---
+++ StringReverser.cpp
@@ -0,0 +1,12 @@
+//
+// Created by hodyah on 12/01/2020.
+//
+
+#include "StringReverser.h"
+
+StringReverser::StringReverser(std::string) {
+    throw "not implemented";
+}
+std::string* StringReverser::solve(std::string *) {
+    throw "not implemented";
+}

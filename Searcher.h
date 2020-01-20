32140149bf18a4cbb7f573aa23b7754c744b3d53
nitsan <nitsanbh@gmail.com>
2020-01-20 23:02:56 +0200

    working on dfs and graph testing

--- Searcher.h
+++ Searcher.h
@@ -6,18 +6,20 @@
 #define SEARCHINGALGOSERVER_SEARCHER_H
 
 #include <Searchable.h>
+
 template <typename Problem, typename Solution>
 class Searcher{
 
 protected:
-    Problem * problem;
 
 public:
-    explicit Searcher(Problem * problem) : problem(problem) {};
+    Problem *problem; // todo make protected
+    explicit Searcher(Problem *problem) : problem(problem) {};
 
     virtual Solution findTheAnswer() = 0;
-    virtual int howNumObjectIOpen() = 0;
-    virtual void popPriorityQueue() = 0;
-    virtual int getPriorityQueueSize() = 0;
+
+//    virtual int howNumObjectIOpen() = 0;
+//    virtual void popPriorityQueue() = 0;
+//    virtual int getPriorityQueueSize() = 0;
 };
 #endif //SEARCHINGALGOSERVER_SEARCHER_H

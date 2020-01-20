daa82aa23e25e0f6471ad3692fbbc76e65bae406
nitasn <46115487+nitasn@users.noreply.github.com>
2020-01-20 20:32:48 +0200

    Create algorithmDFS.h

---
+++ algorithmDFS.h
@@ -0,0 +1,23 @@
+//
+// Created by Nitsan BenHanoch on 20/01/2020.
+//
+
+#ifndef SEARCHINGALGOSERVER_ALGORITHMDFS_H
+#define SEARCHINGALGOSERVER_ALGORITHMDFS_H
+
+#include "Searcher.h"
+#include "SearcherAbstarct.h"
+#include <set>
+#include <list>
+
+template<typename State> class algorithmDFS : public Searcher<Searchable<State>, std::list<State> *>
+{
+    std::set<State> visited;
+
+    std::list<State> *recursivePathFinder(State state);
+
+public:
+    std::list<State> *findTheAnswer() override;
+};
+
+#endif //SEARCHINGALGOSERVER_ALGORITHMDFS_H

8e133d4c2a24b7346a7fb493d521d57bcaac8655
nitsan <nitsanbh@gmail.com>
2020-01-27 19:52:08 +0200

    changed searcher to only accept typename state, also fixed a bug in the matrix init

--- Solver.h
+++ Solver.h
@@ -6,10 +6,11 @@
 #define SEARCHINGALGOSERVER_SOLVER_H
 
 
-template<typename Problem, typename Solution> struct Solver {
-//    virtual Solution *solve(Problem *) = 0;
+template<typename Problem, typename Solution>
+struct Solver
+{
     //todo solver - check if semothing in chcah, or solve him
-    virtual std::vector<Solution>* solve(std::vector<Problem> stringVector) = 0;
+    virtual Solution getSolution(Problem problem) = 0;
 };
 
 

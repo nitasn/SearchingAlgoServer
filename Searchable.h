8611e2d2b9a417b4551b33d0397a904081f59891
nitsan <nitsanbh@gmail.com>
2020-01-27 19:52:54 +0200

    changed searcher to only accept typename state, also fixed a bug in the matrix init

--- Searchable.h
+++ Searchable.h
@@ -6,12 +6,20 @@
 #define SEARCHINGALGOSERVER_SEARCHABLE_H
 
 #include <list>
+
 template<typename State>
-class Searchable{
+class Searchable
+{
 public:
     virtual State getStart() = 0;
+
     virtual State getGoal() = 0;
+
     virtual double getWeight(State t1, State t2) = 0;
+
+    virtual double getMinimalWeight() = 0;
+
     virtual std::list<State> getNeighbors(State object) = 0;
 };
+
 #endif //SEARCHINGALGOSERVER_SEARCHABLE_H
